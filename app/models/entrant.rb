ad80b2c737d6e4f0c6d1abf51770dae27227f486
Christian Rost <chr@baltic-online.de>
2012-12-10 16:08:31 +0100

    Entrants-View

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -1,6 +1,6 @@
 class Entrant < ActiveRecord::Base
   paginates_per 10
-  default_scope order(:date,:category,:title)
+  default_scope order("date DESC", :category, :title)
   
   scope :by_category, ->(categories) {{:conditions =>["category in (?)",categories]}}
   scope :by_date,     ->(dates)      {{:conditions =>["date in (?)",dates]}}
