4c2ef1ebaf270b0ec741666ab0e5345b935a43f9
Christian Rost <chr@baltic-online.de>
2013-01-05 23:51:03 +0100

    SFiles added

--- lib/scraper/boerse_source.rb
+++ lib/scraper/boerse_source.rb
@@ -74,16 +74,19 @@ module Scraper
           begin
             object = SFile.where(:srcid => sfile[:srcid]).first_or_initialize(sfile)
             unless object.new_record?
-              sfile[:other][:changes] = Diffy::Diff.new(object.other[:content], sfile[:other][:content], :context => 1).to_s(:html) if usediffy
+              if (usediffy && object.other[:content] != sfile[:other][:content])
+                sfile[:other][:changes] = Diffy::Diff.new(object.other[:content], sfile[:other][:content], :context => 1).to_s(:html)
+              end
               object.attributes = sfile
             end
-            printf "\t%s %p %s / %s %s: %s [%s]\n",  object.new_record? ? "(NEW)" : (object.changed? ? "(UPD)" : "(OLD)"),
+            printf "\t%s %p %s / %s %s: %s [%s] %p\n",  object.new_record? ? "(NEW)" : (object.changed? ? "(UPD)" : "(OLD)"),
                                                      object.category,
                                                      object.date.strftime("%d.%m.%y %a %H:%M"),
                                                      entry.last_modified.strftime("%d.%m.%y %a %H:%M"),
                                                      object.other[:author],
                                                      object.title,
-                                                     object.srcid
+                                                     object.srcid,
+                                                     object.changed_attributes
             object.save if object.new_record? || object.changed?
           rescue Timeout::Error
             if sfile.include?(:thumbnail)
