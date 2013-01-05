e60e47a3145b1945c0b7b5abcc98384b8e2503db
Christian Rost <chr@baltic-online.de>
2013-01-05 23:19:37 +0100

    SFiles added

--- lib/scraper/boerse_source.rb
+++ lib/scraper/boerse_source.rb
@@ -73,10 +73,10 @@ module Scraper
           usediffy = true
           retries  = 0
           begin
-            newobject = (oldobject = SFile.where(:srcid => sfile[:srcid])).first_or_initialize(sfile)
+            newobject = (oldobjects = SFile.where(:srcid => sfile[:srcid])).first_or_initialize(sfile)
               
-            if (oldobject) && newobject.other[:content] != oldobject.first.other[:content]
-              puts Diffy::Diff.new(newobject.other[:content], oldobject.first.other[:content], :context => 1).to_s(:color)
+            if (!oldobjects.empty? && newobject.other[:content] != oldobjects.first.other[:content])
+              puts Diffy::Diff.new(newobject.other[:content], oldobjects.first.other[:content], :context => 1).to_s(:color)
             end
             
               # if (changed = (dbsfile.other[:content] != sfile[:other][:content]))
