8cb3ea994a94cafa4f807e687c21cbdd08f9bd57
Christian Rost <chr@baltic-online.de>
2013-01-05 21:34:30 +0100

    SFiles added

--- app/helpers/entrants_helper.rb
+++ app/helpers/entrants_helper.rb
@@ -34,7 +34,7 @@ module EntrantsHelper
   
   
   def sfile_content(sfile)
-    content = sfile.other[:content]
+    #content = sfile.other[:content]
     #content = Nokogiri::HTML(sfile.other[:content])
     #content.xpath("//img").each { |image|  image.remove if image.attributes['src'].value == sfile.other[:thumbnail] } unless sfile.other[:thumbnail].nil?
     #content.xpath("//a").each { |link| link.set_attribute('href',"http://www.boerse.bz/out/?url=#{$1}") if link.attributes['href'].value =~ /(http:\/\/.*.gulli.bz\/.*)/ }
