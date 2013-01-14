b7bcef0afe26d14411694e8f11521556b952d1fc
Christian Rost <chr@baltic-online.de>
2013-01-14 10:43:25 +0100

    RSS-Update

--- app/helpers/entrants_helper.rb
+++ app/helpers/entrants_helper.rb
@@ -39,7 +39,7 @@ module EntrantsHelper
     content.xpath("//a").each { |link| link.set_attribute('href',"http://www.boerse.bz/out/?url=#{$1}") if link.attributes['href'].value =~ /(http:\/\/.*.gulli.bz\/.*)/ }
     content.xpath("//div[@class='body-spoiler']").each_with_index do |spoiler,index| 
       unless sfile.other[:spoiler].nil? || sfile.other[:spoiler].empty? || sfile.other[:spoiler][index].nil?
-        webspoiler = Nokogiri::HTML(sfile.other[:spoiler][:index]).at("div[@class='body-spoiler']")
+        webspoiler = Nokogiri::HTML(sfile.other[:spoiler][index]).at("div[@class='body-spoiler']")
         webspoiler.set_attribute('style','')
         spoiler = webspoiler
       end
