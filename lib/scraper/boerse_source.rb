e58390620c869a18324b4e3da96312ef636f0b0c
Christian Rost <chr@baltic-online.de>
2013-01-22 13:35:31 +0100

    soundex

--- lib/scraper/boerse_source.rb
+++ lib/scraper/boerse_source.rb
@@ -155,7 +155,7 @@ module Scraper
           if entry.summary =~ /Bild: (http:\/\/[^ ]*)/
             sfile[:imageurl] = $1
           end
-          if entry.title =~ /^(.*?)((19|20)\d{2})/
+          if entry.title =~ /^([^\(]*?)[ \(]*((19|20)\d{2})/
             sfile[:other] = {} if sfile[:other].nil?
             sfile[:other][:movietitle] = $1
             sfile[:other][:movieyear] = $2
