aa57840aa94451e30adda21fbc34a0413c4b7f69
Christian Rost <chr@baltic-online.de>
2013-01-27 21:36:54 +0100

    single_token accessush

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :require_user,           :except => [:feed,:feedlinks,:downloadlinks] # always require login to access any content at this site
-  #before_filter :require_http_auth_user, :only   => [:feed,:feedlinks]                # http_auth for all feeds or use ?user_credentials=<single_access_token>. only downloadlinks-url is public accessible
+  before_filter :require_http_auth_user, :only   => [:feed,:feedlinks]                # http_auth for all feeds or use ?user_credentials=<single_access_token>. only downloadlinks-url is public accessible
   layout :resolve_layout
   helper_method :current_user_session, :current_user, :controller?, :action?, :scope?
     
